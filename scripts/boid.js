35f37ab525b90635b0f5fae5c3d7016188003234
KrzysztofN <krzysztofn1993@gmail.com>
2019-05-19 13:27:43 +0300

    update on rendering

--- scripts/boid.js
+++ scripts/boid.js
@@ -1,48 +1,59 @@
 class Boid {
-
     constructor() {
         this.position = createVector(random(width), random(height));
         this.velocity = p5.Vector.random2D();
         this.velocity.setMag(random(0.5, 1.5));
         this.acc = createVector();
-        this.maxSpeed = random(2.5, 4.5);
+        this.maxSpeed = random(3.5, 3.5);
         this.maxForce = 1.4;
         this.repelingMult = 1;
         this.avgPosMult = 1;
         this.avgSpeedMult = 1;
         this.perception = 25;
+        this.size = 2;
     }
 
-    boundaries () {
-        if (this.position.x > width ) {
+    boundaries() {
+        if (this.position.x > width) {
             this.position.x = 0;
-        } else if (this.position.x < 0 ) {
+        } else if (this.position.x < 0) {
             this.position.x = width;
         }
         if (this.position.y > height) {
             this.position.y = 0;
-        } else if (this.position.y < 0 ) {
+        } else if (this.position.y < 0) {
             this.position.y = height;
         }
     }
 
-    show (str) {
+    show(str) {
         strokeWeight(str);
-        stroke(255);
-        point(this.position.x, this.position.y)
+        stroke(200);
+        fill(127);
+        push();
+        translate(this.position.x, this.position.y);
+        let angle = this.velocity.heading() + radians(90);
+        angleMode(DEGREES);
+        rotate((angle * 180) / PI);
+        beginShape(TRIANGLES);
+        vertex(0, 0);
+        vertex(-this.size, 2.5 * this.size);
+        vertex(this.size, 2.5 * this.size);
+        endShape();
+        pop();
     }
 
-    update (rep, speed, pos) {
+    update(rep, speed, pos) {
         this.repelingMult = rep;
         this.avgPosMult = pos;
         this.avgSpeedMult = speed;
-        this.position.add(this.velocity);
         this.velocity.add(this.acc);
         this.velocity.setMag(this.maxSpeed);
+        this.position.add(this.velocity);
     }
 
-    flock (boids) {
-        this.acc.set(0,0);
+    flock(boids) {
+        this.acc.set(0, 0);
         let forces = this.align(boids);
         forces.repeling.mult(this.repelingMult);
         this.acc.add(forces.repeling);
@@ -52,13 +63,13 @@ class Boid {
         this.acc.add(forces.avgPos);
     }
 
-    align (boids) {
+    align(boids) {
         let avgSpeed = createVector();
         let avgPos = createVector();
         let repeling = createVector();
         let nearObjects = 0;
         for (let index = 0; index < boids.length; index++) {
-            let currentDistance = this.position.dist(boids[index].userData.position)
+            let currentDistance = this.position.dist(boids[index].userData.position);
             if (this != boids[index].userData) {
                 nearObjects++;
                 avgSpeed.add(boids[index].userData.velocity);
@@ -67,7 +78,6 @@ class Boid {
             }
         }
         if (nearObjects != 0) {
-
             avgSpeed.div(nearObjects);
             avgSpeed.sub(this.velocity);
             avgSpeed.limit(this.maxForce);
@@ -77,7 +87,7 @@ class Boid {
             avgPos.setMag(this.maxSpeed);
             avgPos.sub(this.velocity);
             avgPos.limit(this.maxForce);
-            
+
             repeling.div(nearObjects);
             repeling.setMag(this.maxSpeed);
             repeling.sub(this.velocity);
